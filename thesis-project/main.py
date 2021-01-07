9516be5fdf8416428c4fe606541e7a88722dc11f
AvramPop <daniel.avram.pop@gmail.com>
2021-01-07 11:51:28 +0200

    done up until lemmatization but doesn't work yet

--- thesis-project/main.py
+++ thesis-project/main.py
@@ -1,7 +1,9 @@
 import re
 import string
+import stanza
 
 
+# read a file to an array in which each item is a line from that respective file
 def read_file_line_by_line(filename):
     content = []
     with open(filename) as f:
@@ -10,10 +12,12 @@ def read_file_line_by_line(filename):
     return content
 
 
+# join all elements of an array, separated by a space
 def concatenate_verses(text):
     return ' '.join(text)
 
 
+# parse a string to an array in which each element is a group of sentences_in_batch sentences
 def parse_text_to_sentences(text, sentences_in_batch):
     result = []
     sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=[.?!])\s', text)  # this regex can be improved
@@ -26,24 +30,48 @@ def parse_text_to_sentences(text, sentences_in_batch):
     return result
 
 
+# remove all punctuation from a text
 def remove_punctuation(text):
-    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.translate(str.maketrans('', '', string.punctuation + r"—"))
     return text
 
 
+# split a given text to an array in which each element is a word
 def split_in_tokens(text):
     text = text.split()
     return text
 
 
+# remove all stop words from given list of words, using as reference NLTK data
+def remove_stop_words(tokens):
+    stop_words = read_file_line_by_line("/home/dani/Desktop/code/scoala/licenta/bachelor-thesis/thesis-project/resources/util/stop-words.txt")
+    return [word for word in tokens if word not in stop_words]
+
+
+# lowercase all the words given
+def tokens_to_lower_case(tokens):
+    return [word.casefold() for word in tokens]
+
+
+# lemmatize given list of words using Stanza (Standford NLP)
+def lemmatize(words):
+    nlp = stanza.Pipeline(lang='en', processors='tokenize', tokenize_pretokenized=True)
+    doc = nlp([words])
+    lemmatized_sentence = doc.sentences[0]
+    return [word.text for word in lemmatized_sentence.tokens]
+
+
 def main():
     lines = read_file_line_by_line("/home/dani/Desktop/code/scoala/licenta/bachelor-thesis/thesis-project/resources/philippians/3/esv.txt")
     text = concatenate_verses(lines)
-    sentences = parse_text_to_sentences(text, 1)
+    sentences = parse_text_to_sentences(text, 2)
     first_sentence = sentences[0]
     first_sentence_without_punctuation = remove_punctuation(first_sentence)
     tokens = split_in_tokens(first_sentence_without_punctuation)
-    print(tokens)
+    casefold_tokens = tokens_to_lower_case(tokens)
+    relevant_tokens = remove_stop_words(casefold_tokens)
+    lemmatized_tokens = lemmatize(relevant_tokens)
+    print(lemmatized_tokens)
 
 
 main()
