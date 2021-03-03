02162b9c8b9a7599cd6bbc3c827449501a713b9d
AvramPop <daniel.avram.pop@gmail.com>
2021-03-03 17:54:15 +0200

    added graphs with clustering algorithm

--- thesis-project/graph.py
+++ thesis-project/graph.py
@@ -6,29 +6,34 @@ import networkx
 import preprocessing
 
 
-def generate_similarity_matrix_for_graph_algorithm(sentences_as_embeddings):
+def generate_similarity_matrix_for_graph_algorithm(sentences_as_embeddings, threshold):
     number_of_sentences = len(sentences_as_embeddings)
     similarity_matrix = np.zeros([number_of_sentences, number_of_sentences])
     for i, row_embedding in enumerate(sentences_as_embeddings):
         for j, column_embedding in enumerate(sentences_as_embeddings):
-            similarity_matrix[i][j] = 1 - spatial.distance.cosine(row_embedding, column_embedding)
+            if i == j:
+                similarity_matrix[i][j] = 0
+            else:
+                value = 1 - spatial.distance.cosine(row_embedding, column_embedding)
+                similarity_matrix[i][j] = value if value > threshold else 0
     return similarity_matrix
 
 
-def get_clusters(similarity_matrix, threshold):
-    infomap = im.Infomap("--two-level")
+def get_clusters(similarity_matrix):
+    infomap = im.Infomap()
     for i in range(len(similarity_matrix)):
         for j in range(len(similarity_matrix)):
-            if similarity_matrix[i][j] > threshold:
+            if similarity_matrix[i][j] > 0:
                 infomap.add_link(i, j, similarity_matrix[i][j])
     infomap.run()
-    print(f"Found {infomap.num_top_modules} modules with codelength: {infomap.codelength}")
-    print("Result")
-    print("\n#node module")
+    clusters = {}
     for node in infomap.tree:
         if node.is_leaf:
-            print(node.node_id, node.module_id)
-    return infomap
+            if node.module_id in clusters:
+                clusters[node.module_id].append(node.node_id)
+            else:
+                clusters[node.module_id] = [node.node_id]
+    return clusters
 
 
 def indexes_from_pagerank(scores, summary_size):
@@ -39,13 +44,69 @@ def summary_from_indexes(sentence_indexes_sorted_by_score, text_as_sentences_wit
     return preprocessing.concatenate_text_as_array([text_as_sentences_without_footnotes[i] for i in sentence_indexes_sorted_by_score])
 
 
-def generate_summary_graph(sentences_as_embeddings, text_as_sentences_without_footnotes, summary_size, threshold=0):
+def get_clusters_with_max_coefficients(clusters, clustering_coefficients):
+    result = {}
+    for cluster, nodes in clusters.items():
+        maxi = 0
+        for node in nodes:
+            if clustering_coefficients[node] > maxi:
+                maxi = clustering_coefficients[node]
+        result[cluster] = maxi
+    return result
+
+
+def best_from_cluster(clustering_coefficients_for_each_node, clusters, cluster_number):
+    nodes = {k: v for k, v in clustering_coefficients_for_each_node.items() if k in clusters[cluster_number]}
+    return list({k: v for k, v in sorted(nodes.items(), key=lambda item: item[1], reverse=True)}.keys())[0]
+
+
+def remove_best(best, cluster_number, clusters, clustering_coefficients_for_each_node):
+    clusters[cluster_number] = [item for item in clusters[cluster_number] if item != best]
+    clustering_coefficients_for_each_node.pop(best)
+
+
+def generate_summary_graph(sentences_as_embeddings, text_as_sentences_without_footnotes, summary_size, threshold=0.35):
+    start_time = time.time()
+    similarity_matrix = generate_similarity_matrix_for_graph_algorithm(sentences_as_embeddings, threshold)
+    graph = networkx.from_numpy_array(similarity_matrix)
+    clustering_coefficients_for_each_node = networkx.clustering(graph)
+    average_clustering_coefficient = networkx.average_clustering(graph)
+    clusters = get_clusters(similarity_matrix)
+    # print(coefficients_of_clusters)
+    i = 0
+    solution = []
+    while True:
+        coefficients_of_clusters = get_coefficients_for_clusters_sorted(clustering_coefficients_for_each_node, clusters)
+        number_of_clusters = len(coefficients_of_clusters)
+        for cluster_number in list(coefficients_of_clusters):
+            if coefficients_of_clusters[cluster_number] < average_clustering_coefficient:
+                del coefficients_of_clusters[cluster_number]
+                clusters.pop(cluster_number)
+            elif len(solution) < summary_size:
+                best = best_from_cluster(clustering_coefficients_for_each_node, clusters, cluster_number)
+                solution.append(best)
+                remove_best(best, cluster_number, clusters, clustering_coefficients_for_each_node)
+        if len(solution) >= summary_size:
+            break
+    solution.sort()
+    summary = summary_from_indexes(solution, text_as_sentences_without_footnotes)
+    print("Graphs algorithm took ", time.time() - start_time, "s")
+    return summary
+
+
+def get_coefficients_for_clusters_sorted(clustering_coefficients_for_each_node, clusters):
+    coefficients_of_clusters = get_clusters_with_max_coefficients(clusters, clustering_coefficients_for_each_node)
+    coefficients_of_clusters = {k: v for k, v in
+                                sorted(coefficients_of_clusters.items(), key=lambda item: item[1], reverse=True)}
+    return coefficients_of_clusters
+
+
+def generate_summary_graph_text_rank(sentences_as_embeddings, text_as_sentences_without_footnotes, summary_size):
     start_time = time.time()
     similarity_matrix = generate_similarity_matrix_for_graph_algorithm(sentences_as_embeddings)
-    # get_clusters(similarity_matrix, threshold)
     graph = networkx.from_numpy_array(similarity_matrix)
-    scores = networkx.pagerank(graph, max_iter=20000)
+    scores = networkx.pagerank(graph)
     sentence_indexes_sorted_by_score = indexes_from_pagerank(scores, summary_size)
     summary = summary_from_indexes(sentence_indexes_sorted_by_score, text_as_sentences_without_footnotes)
-    print("Graphs algorithm took ", time.time() - start_time, "s")
+    print("Text rank algorithm took ", time.time() - start_time, "s")
     return summary
